a57560ce550e25b55f83239982f03749e13b8735
zhangxinpeng201 <1850324661@qq.com>
2020-03-31 22:13:29 +0800

    配置文件配置debug模式，可以看到自动注入的组件，使用抽象层日志slf4j以及实现日志logback

--- src/main/java/com/zxp/springBootStarter.java
+++ src/main/java/com/zxp/springBootStarter.java
@@ -1,5 +1,7 @@
 package com.zxp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
@@ -7,7 +9,12 @@ import org.springframework.context.annotation.ImportResource;
 //@ImportResource(locations = {"classpath:bean.xml"})//让我们自己写的配置文件bean.xml生效
 @SpringBootApplication
 public class springBootStarter {
+
+
+
     public static void main(String[] args) {
+        Logger logger =LoggerFactory.getLogger(springBootStarter.class);
+        logger.info("hello ---------------springboot");
         SpringApplication.run(springBootStarter.class,args);
     }
 }
